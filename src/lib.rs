06395737b10e38a45b73f0da9d430139d6a4c03e
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2025-05-25 15:12:23 +0200

    feat: make Symbol generic

--- src/lib.rs
+++ src/lib.rs
@@ -6,51 +6,110 @@ use bumpalo::Bump;
 use dashmap::{DashMap, RwLock};
 use std::{collections::hash_map::RandomState, hash::BuildHasher, num::NonZeroU32};
 
-/// Unique identifier for a string in an [`Interner`].
-#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+/// Default unique identifier for a string in an [`Interner`].
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Symbol(NonZeroU32);
 
 impl Symbol {
     /// Creates a new `Symbol` from a `u32`.
+    ///
+    /// # Panics
+    ///
+    /// Panics if `id` is `u32::MAX`.
     #[inline]
     pub fn new(id: u32) -> Self {
-        Self(NonZeroU32::new(id.wrapping_add(1)).unwrap())
+        Self::try_new(id).unwrap()
+    }
+
+    /// Tries to create a new `Symbol` from a `u32`.
+    #[inline]
+    pub fn try_new(id: u32) -> Option<Self> {
+        if id < u32::MAX {
+            // SAFETY: `id` is less than `u32::MAX`.
+            Some(unsafe { Self::new_unchecked(id) })
+        } else {
+            None
+        }
     }
 
     /// Creates a new `Symbol` from a `usize`.
+    ///
+    /// # Panics
+    ///
+    /// Panics if `id` is greater than or equal to `u32::MAX`.
     #[inline]
     pub fn from_usize(id: usize) -> Self {
-        Self::new(id as u32)
+        Self::try_from_usize(id).unwrap()
+    }
+
+    /// Tries to create a new `Symbol` from a `usize`.
+    #[inline]
+    pub fn try_from_usize(id: usize) -> Option<Self> {
+        if id < u32::MAX as usize {
+            // SAFETY: `id` is less than `u32::MAX`.
+            Some(unsafe { Self::new_unchecked(id as u32) })
+        } else {
+            None
+        }
+    }
+
+    /// Creates a new `Symbol` from a `u32` without checking if it is valid.
+    #[inline]
+    pub unsafe fn new_unchecked(id: u32) -> Self {
+        Self(unsafe { NonZeroU32::new_unchecked(id.unchecked_add(1)) })
     }
 
     /// Returns the `u32` value of the `Symbol`.
     #[inline]
     pub fn get(self) -> u32 {
-        self.0.get().wrapping_sub(1)
+        // SAFETY: `NonZeroU32` is guaranteed to be non-zero, so we can safely
+        // subtract 1 to get the original id.
+        unsafe { self.0.get().unchecked_sub(1) }
     }
 
     /// Returns the `usize` value of the `Symbol`.
     #[inline]
-    pub fn get_usize(self) -> usize {
+    pub fn to_usize(self) -> usize {
         self.get() as usize
     }
 }
 
+/// Trait for types that can be used as symbols in an `Interner`.
+pub trait InternerSymbol: Sized + Copy + std::hash::Hash + Eq {
+    /// Tries to create a new `Symbol` from a `usize`.
+    fn try_from_usize(id: usize) -> Option<Self>;
+
+    /// Converts the `Symbol` to a `usize`.
+    fn to_usize(self) -> usize;
+}
+
+impl InternerSymbol for Symbol {
+    #[inline]
+    fn try_from_usize(id: usize) -> Option<Self> {
+        Self::try_from_usize(id)
+    }
+
+    #[inline]
+    fn to_usize(self) -> usize {
+        self.to_usize()
+    }
+}
+
 /// `str -> Symbol` interner.
-/// The `str` is stored in `strs` instead of in the map to avoid double hashing.
+/// The hash is also stored to avoid double hashing.
 ///
 /// This uses `NoHasher` because we want to store the `hash_builder`
 /// outside of the lock, and to avoid hashing twice on insertion.
-type Map = DashMap<u64, Symbol, NoHasherBuilder>;
-type MapKey = (u64, Symbol);
+type Map<S> = DashMap<u64, S, NoHasherBuilder>;
+type MapKey<S> = (u64, S);
 
 /// String interner.
 ///
 /// See the [module docs][self] for more details.
-pub struct Interner<S = RandomState> {
-    map: Map,
+pub struct Interner<S = Symbol, H = RandomState> {
+    map: Map<S>,
     inner: RwLock<Inner>,
-    hash_builder: S,
+    hash_builder: H,
 }
 
 struct Inner {
@@ -68,28 +127,29 @@ impl Default for Interner {
     }
 }
 
-impl Interner {
-    /// Creates a new, empty `Interner`.
+impl Interner<Symbol, RandomState> {
+    /// Creates a new, empty `Interner` with the default symbol and hasher.
     #[inline]
     pub fn new() -> Self {
         Self::with_capacity(0)
     }
 
-    /// Creates a new `Interner` with the given capacity.
+    /// Creates a new `Interner` with the given capacity and default symbol and hasher.
     #[inline]
     pub fn with_capacity(capacity: usize) -> Self {
         Self::with_capacity_and_hasher(capacity, Default::default())
     }
 }
 
-impl<S: BuildHasher> Interner<S> {
+impl<S: InternerSymbol, H: BuildHasher> Interner<S, H> {
     /// Creates a new `Interner` with the given custom hasher.
-    pub fn with_hasher(hash_builder: S) -> Self {
+    #[inline]
+    pub fn with_hasher(hash_builder: H) -> Self {
         Self::with_capacity_and_hasher(0, hash_builder)
     }
 
     /// Creates a new `Interner` with the given capacitiy and custom hasher.
-    pub fn with_capacity_and_hasher(capacity: usize, hash_builder: S) -> Self {
+    pub fn with_capacity_and_hasher(capacity: usize, hash_builder: H) -> Self {
         let map = Map::with_capacity_and_hasher(capacity, Default::default());
         let strs = Vec::with_capacity(capacity);
         Self { map, inner: RwLock::new(Inner { strs, arena: Bump::new() }), hash_builder }
@@ -114,7 +174,7 @@ impl<S: BuildHasher> Interner<S> {
     /// If `s` outlives `self`, like `&'static str`, prefer using
     /// [`intern_static`](Self::intern_static), as it will not allocate the string on the heap.
     #[inline]
-    pub fn intern(&self, s: &str) -> Symbol {
+    pub fn intern(&self, s: &str) -> S {
         self.do_intern(s, alloc)
     }
 
@@ -128,7 +188,7 @@ impl<S: BuildHasher> Interner<S> {
     ///
     /// By taking `&mut self`, this never acquires any locks.
     #[inline]
-    pub fn intern_mut(&mut self, s: &str) -> Symbol {
+    pub fn intern_mut(&mut self, s: &str) -> S {
         self.do_intern_mut(s, alloc)
     }
 
@@ -137,7 +197,7 @@ impl<S: BuildHasher> Interner<S> {
     /// Note that this only requires that `s` outlives `self`, which means we can avoid allocating
     /// the string.
     #[inline]
-    pub fn intern_static<'a, 'b: 'a>(&'a self, s: &'b str) -> Symbol {
+    pub fn intern_static<'a, 'b: 'a>(&'a self, s: &'b str) -> S {
         self.do_intern(s, no_alloc)
     }
 
@@ -148,18 +208,23 @@ impl<S: BuildHasher> Interner<S> {
     ///
     /// By taking `&mut self`, this never acquires any locks.
     #[inline]
-    pub fn intern_mut_static<'a, 'b: 'a>(&'a mut self, s: &'b str) -> Symbol {
+    pub fn intern_mut_static<'a, 'b: 'a>(&'a mut self, s: &'b str) -> S {
         self.do_intern_mut(s, no_alloc)
     }
 
     /// Maps a `Symbol` to its string.
+    ///
+    /// # Panics
+    ///
+    /// Panics if `Symbol` is out of bounds of this `Interner`. You should only use `Symbol`s
+    /// created by this `Interner`.
     #[inline]
-    pub fn resolve(&self, sym: Symbol) -> &str {
-        self.inner.read().strs[sym.get() as usize]
+    pub fn resolve(&self, sym: S) -> &str {
+        unsafe { self.inner.read().strs.get_unchecked(sym.to_usize()) }
     }
 
     #[inline]
-    fn do_intern(&self, s: &str, alloc: impl FnOnce(&mut Bump, &str) -> &'static str) -> Symbol {
+    fn do_intern(&self, s: &str, alloc: impl FnOnce(&mut Bump, &str) -> &'static str) -> S {
         let hash = self.hash(s);
         let shard_idx = self.map.determine_shard(hash as usize);
         let shard = &*self.map.shards()[shard_idx];
@@ -179,11 +244,7 @@ impl<S: BuildHasher> Interner<S> {
     }
 
     #[inline]
-    fn do_intern_mut(
-        &mut self,
-        s: &str,
-        alloc: impl FnOnce(&mut Bump, &str) -> &'static str,
-    ) -> Symbol {
+    fn do_intern_mut(&mut self, s: &str, alloc: impl FnOnce(&mut Bump, &str) -> &'static str) -> S {
         let hash = self.hash(s);
         let shard_idx = self.map.determine_shard(hash as usize);
         let shard = &mut *self.map.shards_mut()[shard_idx];
@@ -215,28 +276,28 @@ impl std::hash::Hasher for NoHasher {
 }
 
 #[inline]
-fn insert_vacant(
+fn insert_vacant<S: InternerSymbol>(
     inner: &mut Inner,
     s: &str,
     hash: u64,
-    e: hashbrown::hash_table::VacantEntry<'_, MapKey>,
+    e: hashbrown::hash_table::VacantEntry<'_, MapKey<S>>,
     alloc: impl FnOnce(&mut Bump, &str) -> &'static str,
-) -> Symbol {
+) -> S {
     let s = alloc(&mut inner.arena, s);
-    let new_sym = Symbol::from_usize(inner.strs.len());
+    let new_sym = S::try_from_usize(inner.strs.len()).expect("ran out of symbols");
     inner.strs.push(s);
     e.insert((hash, new_sym));
     new_sym
 }
 
 #[inline]
-fn hasher((hash, _): &MapKey) -> u64 {
+fn hasher<S>((hash, _): &MapKey<S>) -> u64 {
     *hash
 }
 
 #[inline]
-fn mk_eq(hash: u64) -> impl Fn(&MapKey) -> bool + Copy {
-    move |(h, _): &MapKey| *h == hash
+fn mk_eq<S>(hash: u64) -> impl Fn(&MapKey<S>) -> bool + Copy {
+    move |(h, _): &MapKey<S>| *h == hash
 }
 
 #[inline]
